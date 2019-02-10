9584f3b84c56b0f68bab06cba01dba35ba450458
din009 <dineshfriends89@gmail.com>
2019-02-10 13:39:11 +0530

    my jenkin commit

--- src/test/java/thirdTest/ThirdTest.java
+++ src/test/java/thirdTest/ThirdTest.java
@@ -1,26 +1,23 @@
-package gitMaster;
+package thirdTest;
 
 import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
-
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
-public class GitMasterTest {
-
+public class ThirdTest {
 	@Test
 	public void logintest() throws IOException{
-		
 		WebDriver wd = new FirefoxDriver();
 		wd.manage().window().maximize();
 		wd.get("https://www.google.com");
 		File scr = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(scr, new File("C:\\Workspace\\GitMaster\\Screenshot\\firsttest.jpg"));
+		FileUtils.copyFile(scr, new File("C:\\Workspace\\GitMaster\\Screenshot\\thirdtest.jpg"));
 		
 	}
 }
